5b17cb41cfab6cf3aed456eb6e58710da6f92569
RNm-dove <m.rynk.plume@gmail.com>
2018-11-17 17:22:44 +0900

    add genre controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :circles
   post '/circles/ids', to: 'circles#index_ids'
+
+  get 'genres', to: 'genres#index'
 end
